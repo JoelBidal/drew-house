8ea9e96ca1211700a280cccdd5f8967bd38859dd
JBidal <joelbidal1@hotmail.com>
2020-09-01 15:06:02 -0300

    add products from javascript

--- js/main.js
+++ js/main.js
@@ -1,43 +1,77 @@
 // Modal de bienvenida
-setTimeout(function() {
-    document.getElementById('welcome-modal').style.display='flex';
-}, 1000);
+// setTimeout(function() {
+//   document.getElementById('welcome-modal').style.display='flex';
+// }, 1000);
 
 
-// Guardando datos en LocalStorage y creando la variable nombre de usuario para el Home
-var datosUsuario = {"nombre":""}
+// // Guardando datos en LocalStorage y creando la variable nombre de usuario para el Home
+// var datosUsuario = {"nombre":""}
 
-const enviarUsuario = document.getElementById('enviar');
+// const enviarUsuario = document.getElementById('enviar');
 
-enviarUsuario.addEventListener("click", function(event){
-    event.preventDefault()
-    datosUsuario.nombre = document.getElementById('nombre').value;
-    document.getElementById('welcome-modal').style.display='none';
-    localStorage.setItem('datosModal', JSON.stringify(datosUsuario))
+// enviarUsuario.addEventListener("click", function(event){
+//   event.preventDefault()
+//   datosUsuario.nombre = document.getElementById('nombre').value;
+//   document.getElementById('welcome-modal').style.display='none';
+//   localStorage.setItem('datosModal', JSON.stringify(datosUsuario))
 
-    var nombreUsuario = datosUsuario.nombre.toLowerCase() 
-    document.getElementById('name').innerText = nombreUsuario;
-    console.log(nombreUsuario);
-})
+//   var nombreUsuario = datosUsuario.nombre.toLowerCase() 
+//   document.getElementById('name').innerText = nombreUsuario;
+//   console.log(nombreUsuario);
+// })
 
-// Creando objetos
-var colores = ['white', 'beige', 'black', 'red', 'blue'];
-var talles = ['XS', 'S', 'M', 'L', 'XL'];
 
-function Producto(talles, colores, stock){
-    this.talles = talles;
-    this.colores = colores;
-    this.stock = stock;
-} 
+// Datos con los productos de la tienda
+var productosJSON = `[
+  {"nombre":"secret ss tee -white", "precio":48.00, "imagen":"../assets/img/clothes/00.jpg"},
+  {"nombre":"corduroy ss shirt - chocolate", "precio":148.00, "imagen":"../assets/img/clothes/01.jpg"},
+  {"nombre":"corduroy shorts - chocolate", "precio":138.00, "imagen":"../assets/img/clothes/02.jpg"},
+  {"nombre":"striped mascot socks - black/golden", "precio":20.00, "imagen":"../assets/img/clothes/03.jpg"},
+  {"nombre":"distressed jean - vintage indigo", "precio":278.00, "imagen":"../assets/img/clothes/04.jpg"},
+  {"nombre":"mascto skate deck - iceberg roses", "precio":58.00, "imagen":"../assets/img/clothes/05.jpg"},
+  {"nombre":"mixed media trucker jacket - bleached", "precio":448.00, "imagen":"../assets/img/clothes/06.jpg"},
+  {"nombre":"drew house space sticker sheet", "precio":10.00, "imagen":"../assets/img/clothes/07.jpg"},
+  {"nombre":"dinodrew skate deck - burnt oronge", "precio":58.00, "imagen":"../assets/img/clothes/08.jpg"}
+]`;
 
-var producto1 = new Producto (talles, colores, true);
-var producto2 = new Producto (talles, colores, true);
-var producto3 = new Producto (talles, colores, true);
-var producto4 = new Producto (talles, colores, true);
-var producto5 = new Producto (talles, colores, true);
-var producto6 = new Producto (talles, colores, true);
-var producto7 = new Producto (talles, colores, true);
-var producto8 = new Producto (talles, colores, true);
-var producto9 = new Producto (talles, colores, true);
+class Producto {
+  constructor(nombre, precio, imagen) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.imagen = imagen;
+  }
+}
+  
+var productos;
 
-var productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9,];
+window.onload = () => {
+  let datosJSON = JSON.parse(productosJSON);
+  productos = datosJSON.map((object) => {
+    return new Producto(object.nombre, object.precio, object.imagen);
+  });
+  
+  const productosColeccion = document.getElementById('products');
+  
+  productos.forEach((producto) => {
+    productosColeccion.innerHTML += crearProducto(producto);
+  });
+};
+
+
+// Agregando los productos al HTML
+function crearProducto(producto) {
+  return `<div class="product-00 unity">
+  <a href="../views/product.html"><img src="${producto.imagen}" alt="short"></a>
+  <h2 class="name-product">${producto.nombre}</h2>
+  <span class="price-product">$${producto.precio}.00</span>
+  </div>`;
+}
+
+// Botón en product.html para agregar al carrito
+let button = document.querySelector('#addCart');
+
+button.addEventListener("click", () => {
+  let counter = document.getElementById('cart-price');
+  let currentValue = counter.innerHTML;
+  counter.innerHTML = parseInt(currentValue) + 1;
+});
