bac11959b264a0bb3e3c259fd8b4275da3228dc1
JBidal <joelbidal1@hotmail.com>
2020-09-03 16:02:53 -0300

    add modal & filter select

--- js/index.js
+++ js/index.js
@@ -3,19 +3,48 @@ setTimeout(function() {
   document.getElementById('welcome-modal').style.display='flex';
 }, 600);
 
-
-// Guardando datos en LocalStorage y creando la variable nombre de usuario para el Home
-var datosUsuario = {"nombre":""}
-
+window.onload = function() {
+  var datosUsuario;
+  if (localStorage.getItem(datosUsuario) == true){
+    alert('existe')
+  }
+  
 const enviarUsuario = document.getElementById('enviar');
 
 enviarUsuario.addEventListener("click", function(event){
   event.preventDefault()
-  datosUsuario.nombre = document.getElementById('nombre').value;
+  datosUsuario = document.getElementById('nombre').value;
   document.getElementById('welcome-modal').style.display='none';
-  localStorage.setItem('datosModal', JSON.stringify(datosUsuario))
+  localStorage.setItem('datosUsuario', datosUsuario)
 
-  var nombreUsuario = datosUsuario.nombre.toLowerCase() 
+  var nombreUsuario = datosUsuario.toLowerCase() 
   document.getElementById('name').innerText = nombreUsuario;
   console.log(nombreUsuario);
 })
+};
+
+
+
+// window.onload = function() {
+//   if('datosModal' in localStorage){
+//   document.getElementById('welcome-modal').style.display='none';
+//   } else {
+//     document.getElementById('welcome-modal').style.display='flex';
+//   }
+
+// // Guardando datos en LocalStorage y creando la variable nombre de usuario para el Home
+// var datosUsuario = {"nombre":""}
+
+// const enviarUsuario = document.getElementById('enviar');
+
+// enviarUsuario.addEventListener("click", function(event){
+//   event.preventDefault()
+//   datosUsuario.nombre = document.getElementById('nombre').value;
+//   document.getElementById('welcome-modal').style.display='none';
+//   localStorage.setItem('datosModal', JSON.stringify(datosUsuario))
+
+//   var nombreUsuario = datosUsuario.nombre.toLowerCase() 
+//   document.getElementById('name').innerText = nombreUsuario;
+//   console.log(nombreUsuario);
+// })
+// };
--- js/shop.js
+++ js/shop.js
@@ -1,21 +1,22 @@
 // Datos con los productos de la tienda
 var productosJSON = `[
-  {"nombre":"secret ss tee -white", "precio":48.00, "imagen":"../assets/img/clothes/00.jpg"},
-  {"nombre":"corduroy ss shirt - chocolate", "precio":148.00, "imagen":"../assets/img/clothes/01.jpg"},
-  {"nombre":"corduroy shorts - chocolate", "precio":138.00, "imagen":"../assets/img/clothes/02.jpg"},
-  {"nombre":"striped mascot socks - black/golden", "precio":20.00, "imagen":"../assets/img/clothes/03.jpg"},
-  {"nombre":"distressed jean - vintage indigo", "precio":278.00, "imagen":"../assets/img/clothes/04.jpg"},
-  {"nombre":"mascto skate deck - iceberg roses", "precio":58.00, "imagen":"../assets/img/clothes/05.jpg"},
-  {"nombre":"mixed media trucker jacket - bleached", "precio":448.00, "imagen":"../assets/img/clothes/06.jpg"},
-  {"nombre":"drew house space sticker sheet", "precio":10.00, "imagen":"../assets/img/clothes/07.jpg"},
-  {"nombre":"dinodrew skate deck - burnt oronge", "precio":58.00, "imagen":"../assets/img/clothes/08.jpg"}
+  {"nombre":"secret ss tee -white", "precio":48.00, "imagen":"../assets/img/clothes/00.jpg", "categoria": "shirts"},
+  {"nombre":"corduroy ss shirt - chocolate", "precio":148.00, "imagen":"../assets/img/clothes/01.jpg", "categoria": "shirts"},
+  {"nombre":"corduroy shorts - chocolate", "precio":138.00, "imagen":"../assets/img/clothes/02.jpg", "categoria": "jeans"},
+  {"nombre":"striped mascot socks - black/golden", "precio":20.00, "imagen":"../assets/img/clothes/03.jpg", "categoria": "accessories"},
+  {"nombre":"distressed jean - vintage indigo", "precio":278.00, "imagen":"../assets/img/clothes/04.jpg", "categoria": "jeans"},
+  {"nombre":"mascot skate deck - iceberg roses", "precio":58.00, "imagen":"../assets/img/clothes/05.jpg", "categoria": "accessories"},
+  {"nombre":"mixed media trucker jacket - bleached", "precio":448.00, "imagen":"../assets/img/clothes/06.jpg", "categoria": "hoodies"},
+  {"nombre":"drew house space sticker sheet", "precio":10.00, "imagen":"../assets/img/clothes/07.jpg", "categoria": "accessories"},
+  {"nombre":"dinodrew skate deck - burnt oronge", "precio":58.00, "imagen":"../assets/img/clothes/08.jpg", "categoria": "accessories"}
 ]`;
 
-class Producto {
-  constructor(nombre, precio, imagen) {
+class producto {
+  constructor(nombre, precio, imagen, categoria) {
     this.nombre = nombre;
     this.precio = precio;
     this.imagen = imagen;
+    this.categoria = categoria;
   }
 }
   
@@ -24,14 +25,31 @@ var productos;
 window.onload = () => {
   let datosJSON = JSON.parse(productosJSON);
   productos = datosJSON.map((object) => {
-    return new Producto(object.nombre, object.precio, object.imagen);
+    return new producto(object.nombre, object.precio, object.imagen, object.categoria);
   });
   
+  const sortBy = document.getElementById('sortBy');
+  const selected = document.getElementById('selectFild');
+
   const productosColeccion = document.getElementById('products');
   
   productos.forEach((producto) => {
     productosColeccion.innerHTML += crearProducto(producto);
   });
+  // --------------
+  sortBy.addEventListener('change', event => {
+  event.preventDefault();
+    
+  let resultadoFiltro = selected.value;
+    
+  let productosElegidos = productos.filter(producto =>{
+    return producto.categoria.toLowerCase().includes(resultadoFiltro.toLowerCase)
+  })
+
+  productosColeccion.innerHTML = '';
+  productosElegidos.forEach(producto => {productosColeccion.innerHTML += crearProducto(producto);})
+  console.log(productosElegidos)
+  })
 };
 
 // Agregando los productos al HTML
@@ -44,27 +62,8 @@ function crearProducto(producto) {
 }
 
 // Arrays con los productos de mi select > option
-var all = [productos];
-var shirts = ['secret ss tee -white', 'corduroy ss shirt - chocolate', ''];
-var jeans = ['corduroy shorts - chocolate','distressed jean - vintage indigo'];
-var hoodies = ['mixed media trucker jacket - bleached'];
-var accessories = ['striped mascot socks - black/golden', 'mascto skate deck - iceberg roses', 'drew house space sticker sheet'];
-
-
-// Seleccionando una catergoria desde el <select>
-selected = document.getElementById('selectFild');
-
-// Necesito mostrar mis categorias dependiendo cual seleccione el usuario
-selected.addEventListener("click", function(){
-  selected = this.value;
-  console.log(selected);
-  
-  const productosColeccion = document.getElementById('products');
-  
-  if (selected == shirts){
-    productosColeccion.innerHTML = shirts;
-  }
-  else{
-    console.log('Sorry :(')
-  }
-});
+// var all = [productos];
+// var shirts = ['secret ss tee -white', 'corduroy ss shirt - chocolate'];
+// var jeans = ['corduroy shorts - chocolate','distressed jean - vintage indigo'];
+// var hoodies = ['mixed media trucker jacket - bleached'];
+// var accessories = ['striped mascot socks - black/golden', 'mascot skate deck - iceberg roses', 'drew house space sticker sheet', 'mascot skate deck - iceberg roses'];
