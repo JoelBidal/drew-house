43b0f14f012d875c6f75307205d8d1c825e19651
JBidal <joelbidal1@hotmail.com>
2020-09-10 11:26:42 -0300

    add jQuery

--- js/index.js
+++ js/index.js
@@ -30,12 +30,12 @@ enviarUsuario.addEventListener("click", function(event){
 };
 
   // Abrir carrito
-  cart = document.getElementById('box-cart')
+  cart = document.getElementById('box-cart');
   cart.addEventListener("click", function(c){
     document.getElementById('cart').style.display='block';
-})
-
-  closeCart = document.getElementById('continue')
+});
+  // Cerrar carrito
+  closeCart = document.getElementById('continue');
   closeCart.addEventListener('click', function(){
     document.getElementById('cart').style.display='none';
-})
+});
